82b59b9cc675c217dfcb45c4690a203b431bbf17
himchan92 <omea92@naver.com>
2025-09-04 13:07:57 +0900

    spring common exception

--- src/main/java/com/sparta/java02/domain/product/service/ProductService.java
+++ src/main/java/com/sparta/java02/domain/product/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.sparta.java02.domain.product.service;
 
+import com.sparta.java02.common.enums.ServiceExceptionCode;
+import com.sparta.java02.common.exception.ServiceException;
 import com.sparta.java02.domain.product.dto.ProductRequest;
 import com.sparta.java02.domain.product.dto.ProductResponse;
 import com.sparta.java02.domain.product.entity.Product;
@@ -21,6 +23,22 @@ public class ProductService {
         return null;
     }
 
+    @Transactional(readOnly = true)
+    public ProductResponse getById(Long id) {
+        //예외발생 시 GlobalExceptionHandler가 가로채서 대신 클라이언트에반환
+        Product product = productRepository.findById(id).orElseThrow(() -> new ServiceException(ServiceExceptionCode.NOT_FOUND_PRODUCT));
+
+        return ProductResponse.builder()
+                .id(product.getId())
+                //.categoryId(product.getCategory())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .stock(product.getStock())
+                .createdAt(product.getCreatedAt())
+                .build();
+    }
+
     @Transactional
     public ProductResponse create(@Valid ProductRequest request) {
         return null;
@@ -34,10 +52,5 @@ public class ProductService {
     @Transactional
     public void delete(Long id) {
         productRepository.deleteById(id);
-    }
-
-    @Transactional(readOnly = true)
-    public ProductResponse getById(Long id) {
-        return null;
     }
 }
