067904df74a108051b735ef37d624ee4833f258d
himchan92 <omea92@naver.com>
2025-11-14 14:20:17 +0900

    1주차 복습완료

--- src/main/java/com/example/demo/domain/user/dto/UserResponse.java
+++ src/main/java/com/example/demo/domain/user/dto/UserResponse.java
@@ -1,25 +1,15 @@
 package com.example.demo.domain.user.dto;
 
-import com.example.demo.domain.user.entity.User;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserResponse {
-
     Long id;
-
     String name;
-
     String email;
-
-    String passwordHash;
-
-    LocalDateTime createdAt;
 }
